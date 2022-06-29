63d5bbe091e1206c7fcadf710caf375deb75eb68
SamadiPour <samadipoor2@gmail.com>
2022-06-29 22:31:43 +0430

    refactor: remove duplicate currencies in Graph model

--- src/bonbast/data.py
+++ src/bonbast/data.py
@@ -123,37 +123,6 @@ class Gold:
 class Graph:
     """ Graph model
     """
-    VALUES = {
-        'usd': 'US Dollar',
-        'eur': 'Euro',
-        'gbp': 'British Pound',
-        'chf': 'Swiss Franc',
-        'cad': 'Canadian Dollar',
-        'aud': 'Australian Dollar',
-        'sek': 'Swedish Krona',
-        'nok': 'Norwegian Krone',
-        'rub': 'Russian Ruble',
-        'thb': 'Thai Baht',
-        'sgd': 'Singapore Dollar',
-        'hkd': 'Hong Kong Dollar',
-        'azn': 'Azerbaijani Manat',
-        'amd': '10 Armenian Dram',
-        'dkk': 'Danish Krone',
-        'aed': 'UAE Dirham',
-        'jpy': '10 Japanese Yen',
-        'try': 'Turkish Lira',
-        'cny': 'Chinese Yuan',
-        'sar': 'Saudi Riyal',
-        'inr': 'Indian Rupee',
-        'myr': 'Malaysian Ringgit',
-        'afn': 'Afghan Afghani',
-        'kwd': 'Kuwaiti Dinar',
-        'iqd': '100 Iraqi Dinar',
-        'bhd': 'Bahraini Dinar',
-        'omr': 'Omani Rial',
-        'qar': 'Qatari Rial',
-    }
-
     def __init__(self, date: str, value: int, currency: str):
         self.date = date
         self.value = value
