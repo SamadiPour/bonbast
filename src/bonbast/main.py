2737ceec096256aed3396b990d6a3eacfb67013c
Amir Hossein <samadipoor2@gmail.com>
2022-07-01 11:53:06 +0430

    feat: implement command line basics (#10)
    * feat: add click library for commandline management
    
    * feat: add commands that we are going to use
    
    * feat(convert-cli): implement logic of converting
    
    * feat(cli): implement showing version with -v/--version
    
    * fix(convert-cli): make currency inputs case insensitive
    
    * feat(convert-cli): add buy and sell only flags
    
    * feat: comment unused commands

--- src/bonbast/main.py
+++ src/bonbast/main.py
@@ -1,11 +1,13 @@
-from datetime import datetime, timedelta
+import click
 from rich.console import Console
 
 try:
+    from .__init__ import *
     from .server import *
     from .storage_manager import *
     from .tables import *
 except:
+    from __init__ import *
     from server import *
     from storage_manager import *
     from tables import *
@@ -36,16 +38,84 @@ def get_prices():
         return response
 
 
-def cli_main():
-    currencies_list, coins_list, golds_list = get_prices()
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    else:
+        click.echo(bonbast_version)
+        ctx.exit()
 
-    currencies_table = get_currencies_table(currencies_list, 2)
-    coins_table = get_coins_table(coins_list)
-    gold_table = get_gold_table(golds_list)
 
-    console = Console()
-    console.print(currencies_table, coins_table, gold_table)
+@click.group(invoke_without_command=True)
+@click.option('-v', '--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        currencies_list, coins_list, golds_list = get_prices()
+
+        currencies_table = get_currencies_table(currencies_list, 2)
+        coins_table = get_coins_table(coins_list)
+        gold_table = get_gold_table(golds_list)
+
+        console = Console()
+        console.print(currencies_table, coins_table, gold_table)
+
+
+# @cli.command()
+# def graph():
+#     click.echo('Graph is not implemented yet')
+
+
+# @cli.command()
+# def live():
+#     click.echo('Live is not implemented yet')
+
+
+@cli.command()
+@click.option('-s', '--source', type=click.Choice(Currency.VALUES, case_sensitive=False))
+@click.option('-d', '--destination', type=click.Choice(Currency.VALUES, case_sensitive=False))
+@click.argument('amount', type=click.FLOAT)
+@click.option('--only-buy', is_flag=True, default=False, help='Only show buy price')
+@click.option('--only-sell', is_flag=True, default=False, help='Only show sell price')
+@click.pass_context
+def convert(ctx, source, destination, amount, only_buy, only_sell):
+    if source is None and destination is None:
+        raise click.BadOptionUsage('', 'Please specify source or destination currency')
+
+    if source is not None and destination is not None:
+        raise click.BadOptionUsage('', 'Don\'t use --source and --destination together')
+
+    if only_buy and only_sell:
+        raise click.BadOptionUsage('', 'Don\'t use --only-buy and --only-sell together')
+
+    currencies_list, _, __ = get_prices()
+    if source is not None:
+        currency = next(currency for currency in currencies_list if currency.code.lower() == source.lower())
+        buy = format_toman(int(amount * currency.buy))
+        sell = format_toman(int(amount * currency.sell))
+    else:
+        currency = next(currency for currency in currencies_list if currency.code.lower() == destination.lower())
+        buy = format_price(amount / currency.sell)
+        sell = format_price(amount / currency.buy)
+
+    if only_buy:
+        click.echo(buy)
+    elif only_sell:
+        click.echo(sell)
+    else:
+        click.echo(f'{buy} / {sell}')
+
+
+# @cli.command()
+# def history():
+#     click.echo('History is not implemented yet')
+
+
+# @cli.command()
+# def export():
+#     click.echo('Export is not implemented yet')
 
 
 if __name__ == '__main__':
-    cli_main()
+    cli()
