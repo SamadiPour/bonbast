d0bd8eca89670c936594ab87ce02b66443735b8a
SamadiPour <samadipoor2@gmail.com>
2024-01-27 15:17:16 +0330

    fix: potential fix for wrong price

--- src/bonbast/server.py
+++ src/bonbast/server.py
@@ -10,9 +10,9 @@ try:
 except ImportError:
     from models import *
 
-BASE_URL = 'https://www.bonbast.com'
-USER_AGENT = 'Mozilla/5.0 (Linux; Android 12.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-             'Chrome/117.0.0.0 Mobile Safari/537.36'
+BASE_URL = 'https://bonbast.com'
+USER_AGENT = ('Mozilla/5.0 (Linux; Android 13; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) '
+              'Chrome/120.0.0.0 Mobile Safari/537.36')
 SELL = '1'
 BUY = '2'
 
@@ -32,26 +32,30 @@ def get_token_from_main_page():
     param dont_raise_error: It's on False by default. If you pass True it won't raise error because of connection problems.
         It will return False instead.
     """
+    cookies = {
+        'cookieconsent_status': 'true',
+        'st_bb': '0',
+    }
     headers = {
         'authority': 'bonbast.com',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
         'cache-control': 'no-cache',
-        'cookie': 'cookieconsent_status=true; st_bb=0',
         'pragma': 'no-cache',
-        'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        'referer': 'https://bonbast.com/',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
         'sec-ch-ua-mobile': '?1',
         'sec-ch-ua-platform': '"Android"',
         'sec-fetch-dest': 'document',
         'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
+        'sec-fetch-site': 'same-origin',
         'sec-fetch-user': '?1',
         'upgrade-insecure-requests': '1',
         'user-agent': USER_AGENT,
     }
 
     try:
-        r = requests.get(BASE_URL, headers=headers)
+        r = requests.get(BASE_URL, headers=headers, cookies=cookies)
         r.raise_for_status()
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
@@ -73,15 +77,18 @@ def get_prices_from_api(token: str) -> Tuple[List[Currency], List[Coin], List[Go
     param dont_raise_error: It's on False by default. If you pass True it won't raise error because of connection problems.
         It will return False instead.
     """
+    cookies = {
+        'cookieconsent_status': 'true',
+        'st_bb': '0',
+    }
     headers = {
         'authority': 'bonbast.com',
         'accept': 'application/json, text/javascript, */*; q=0.01',
-        'accept-language': 'en-US,en;q=0.9,fa;q=0.8',
+        'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'cookie': 'st_bb=0',
         'origin': 'https://bonbast.com',
         'referer': 'https://bonbast.com/',
-        'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
         'sec-ch-ua-mobile': '?1',
         'sec-ch-ua-platform': '"Android"',
         'sec-fetch-dest': 'empty',
@@ -92,7 +99,8 @@ def get_prices_from_api(token: str) -> Tuple[List[Currency], List[Coin], List[Go
     }
 
     try:
-        r = requests.post(f'{BASE_URL}/json', headers=headers, data={'param': token})
+        r = requests.post(f'{BASE_URL}/json', headers=headers, cookies=cookies,
+                          data='param=' + token.replace(',', '%2C'))
         r.raise_for_status()
         r = r.json()
     except requests.exceptions.HTTPError as err:
